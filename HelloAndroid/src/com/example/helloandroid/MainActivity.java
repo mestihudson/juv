036553751dc373a3f229540f3590017404adebf6
Hudson Ferreira Leite <hudson.leite@serpro.gov.br>
2019-05-29 17:05:55 -0300

    android

---
+++ HelloAndroid/src/com/example/helloandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.helloandroid;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+  @Override protected void onCreate(final Bundle state) {
+    super.onCreate(state);
+    setContentView(R.layout.activity_main);
+  }
+}
